279de0209458717fdac49248d5ddf27c7c39f6f2
Esteban Xelhuantzi <a324249@uach.mx>
2021-07-11 17:30:54 -0600

    version 0.1.0

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* Aqui empieza el jsx, no borrar el div de "App" pq sino no jala*/}
+      <header className="App-header">
+        Header. Hola mundo, el erick me la pela 
+        {/* 
+        <p>
+          Este es nomas un parrafo
+          <code>No se que hace esta parte</code> 
+        </p>
+        */}
+        
+      </header>
+      {/* Aqui empieza el jsx, no borrar el div de justo abajo pq sino no jala*/}
+    </div>
+  );
+}
+
+export default App;
