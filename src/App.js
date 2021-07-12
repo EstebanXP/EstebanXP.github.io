1a2ddefefafe42222b690bcaf6414e063d9381d8
Esteban Xelhuantzi <a324249@uach.mx>
2021-07-12 13:13:51 -0600

    carpetas para componentes y css en src

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 
-import './App.css';
-
+import './css/App.css';
+import Navbar from './componentes/Navbar';
+import Poupup from './componentes/PopupChat';
 
 function App() {
   return (
     <div className="App">
+      
       {/* Aqui empieza el jsx, no borrar el div de "App" pq sino no jala*/}
       <header className="App-header">
+      <Navbar></Navbar>
         Header. Hola mundo, el erick me la pela durisimo 
         {/* 
         <p>
@@ -16,7 +19,13 @@ function App() {
         */}
         
       </header>
+      <body>Lorem ipsum dolor sit amet consectetur adipiscing elit scelerisque, felis class augue lectus pharetra aptent vitae, quisque ante molestie sociis phasellus habitasse laoreet. Taciti venenatis natoque iaculis fames pellentesque parturient class curae auctor aptent interdum blandit, mollis aliquet feugiat libero pretium eu lacinia ante integer litora vel, fringilla et laoreet ornare lacus egestas euismod inceptos vitae eros at. Eleifend rhoncus luctus torquent potenti donec class nam, velit euismod ultrices mus mollis praesent interdum, mattis phasellus orci dictum aenean fermentum.
+
+Sagittis ligula tincidunt vehicula sociis rhoncus vel fringilla aliquet, dui etiam venenatis dictumst potenti libero congue scelerisque urna, imperdiet porta integer placerat porttitor nibh metus. Cum fames sociis metus arcu risus ante, diam habitasse feugiat bibendum facilisi eget, pretium ut nibh morbi donec. Natoque tellus egestas interdum semper nam aliquet dapibus donec, id rutrum montes nullam ridiculus facilisis diam velit, parturient venenatis odio sociosqu porttitor habitasse mi.</body>
+      
+      <Poupup></Poupup>
       {/* Aqui empieza el jsx, no borrar el div de justo abajo pq sino no jala*/}
+
     </div>
   );
 }
---
+++ src/componentes/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Navbar extends React.Component{
+    render(){
+        return(
+            <div class="topnav">
+                <a class="active" href="#home">Home</a>
+                <a class="productos" href="#productos">Productos</a>
+                </div>
+        );
+    }
+}
+
+export default Navbar;
