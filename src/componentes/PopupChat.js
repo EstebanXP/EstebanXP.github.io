115f51000094cd4928a04d87e371ea07e06bde49
Esteban Xelhuantzi <a324249@uach.mx>
2021-07-17 13:43:41 -0600

    Descarga de emailjs y arreglos

--- src/componentes/PopupChat.js
+++ src/componentes/PopupChat.js
@@ -17,19 +17,23 @@ class Poupup extends React.Component {
     }
   }
 
+  enviarEmail(nombre,email,asunto){
+    
+  }
+
   render() {
     return (
       <div className="container">
         <button id="bChat" className="btn btn-info btn-chat" type="button" onClick={this.holaMundo}>Chat</button>
         <div className="chatbox" id="chatbox">
           <div className="header">
-            <h2 id="header">Mensaje </h2>
+            <h2 id="header">Contactanos</h2>
           </div>
-          <form className="form-container">
+          <form method="POST" className="form-container">
             <input type="email" required placeholder="Correo electronico" className="field"></input>
             <input type="text" required placeholder="Nombre completo" className="field"></input>
             <textarea type="text" placeholder="Escribe tus dudas" name="msg" required></textarea>
-            <button type="button" className="btn btn-outline-success">Enviar</button>
+            <button type="button" className="btn btn-outline-success" onClick={()=>this.enviarEmail("asdasd","el pepe","Losp peep")}>Enviar</button>
           </form> 
         </div>
         <div>{this.cerrarChat}</div>
