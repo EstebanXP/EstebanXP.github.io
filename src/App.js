98652411960f94777fb98e0b1160e5e1ad29741c
erickdominguez <47347282+erickdominguez@users.noreply.github.com>
2021-07-24 01:25:13 -0600

    iniciacion de easybase

--- src/App.js
+++ src/App.js
@@ -10,30 +10,33 @@ import ImagenFondo from "./componentes/ImagenFondo";
 import FooterPage from "./componentes/Footer";
 //import { Fragment } from "react";
 import Body from "./componentes/Body";
-
+import { EasybaseProvider, Auth } from 'easybase-react'
+import ebconfig from './ebconfig'
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        {/* Aqui empieza el jsx, no borrar el div de "App" pq sino no jala*/}
-        <div>
-          <header className="App-header">
-            <Navbar />
-            <ImagenFondo />
-          </header>
-        </div>
-        <div className="App-Body">
-          <Body />
-          <Poupup />
-        </div>
+    <EasybaseProvider ebconfig={ebconfig}>
+      <Router>
+        <div className="App">
+          {/* Aqui empieza el jsx, no borrar el div de "App" pq sino no jala*/}
+          <div>
+            <header className="App-header">
+              <Navbar />
+              <ImagenFondo />
+            </header>
+          </div>
+          <div className="App-Body">
+            <Body />
+            <Poupup />
+          </div>
 
-        <FooterPage />
-        {/* Aqui termina el jsx, no borrar el div de justo abajo pq sino no jala*/}
-        <Vista />
-      </div>
-    </Router>
+          <FooterPage />
+          {/* Aqui termina el jsx, no borrar el div de justo abajo pq sino no jala*/}
+          <Vista />
+        </div>
+      </Router>
+    </EasybaseProvider>
   );
 }
 
