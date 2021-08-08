05ef58f10a7102e96e2c6febf873303e1dd5af5c
erickdominguez <47347282+erickdominguez@users.noreply.github.com>
2021-08-07 18:48:12 -0600

    AGGGGH QUE HICE (NADA MALO PERO SE ME OLVIDO)

--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
 
 class Navbar extends React.Component{
     render(){
         return(
             <div className="topnav">
-                <Link to="/">
-                    <h4 className="active">Home</h4>
-                </Link>
-                <Link to="/productos">
-                    <h4 className="productos">Productos</h4>
-                </Link>
+                <Container>
+                    <Row>
+                        <Col>
+                            <Link to="/">
+                                <h5 className="home">HOME</h5>
+                            </Link>
+                        </Col>
+                        <Col xs={2}>
+                            <p>hola</p>
+                        </Col>
+                        <Col>
+                            <Link to="/productos">
+                                <h5 className="productos">PRODUCTOS</h5>
+                            </Link>
+                        </Col>
+                    </Row>
+                </Container>
+                
+                
             </div>
         );
     }
