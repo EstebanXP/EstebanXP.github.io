3d21a2128161d3189fbc83b8d32560a5963fc48c
Esteban Xelhuantzi <a324249@uach.mx>
2021-08-16 20:47:20 -0600

    Funcion para agarrar dimensiones

--- src/componentes/Productos.js
+++ src/componentes/Productos.js
@@ -7,12 +7,21 @@ import { Container } from 'react-bootstrap';
 import { Row } from 'react-bootstrap';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button'
-import CarouselProductos from './CarouselProductos';
+import CarouselProductos from './CarouselProductos'; 
+import useWindowDimensions from './funcionVentana';
+
 
 export default function Productos() {
 
     const [easybaseData, setEasybaseData] = useState([]);
+    const { height, width } = useWindowDimensions();
+    //Este es el hook que agarra la altura y anchura, lo cale con la funcion dimensiones, si la necesitas 
+    //en otro componente nomas importa el useWindowDimensions y ya lo puedes jalar con los hooks tambien 
     const { db } = useEasybase();
+
+    var dimensiones = ()=>{
+      console.log(height + "AAAAAAA"+ width); 
+    }
 
     const mounted = async() => {
       const ebData = await db("PRODUCTOS").return().all();
@@ -47,7 +56,6 @@ export default function Productos() {
                 }}>
                   <Button  className="botonProductoVermas" >VER MÁS...</Button>
                 </Link>
-                
 
               </div>
             )}
